a33b5a2b805f8a4a627f8ea208109f5c8f0115b3
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-06-23 12:14:48 +0200

    dropdown button in explore page works but not optimally

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,15 +1,56 @@
 "use client"
 
+import {useState} from "react";
+
 export default function Explore() {
+
+    const [dropdownButtonIsClicked, setDropdownButtonIsClicked] = useState(false);
+
+    function handleDropdownClick() {
+        setDropdownButtonIsClicked(oldValue => !oldValue);
+    }
+
     return (
         <div>
 
             <form>
                 <div className="flex">
                     <label htmlFor="search-dropdown"
-                           className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
-                    <button id="dropdown-button" data-dropdown-toggle="dropdown"
-                            className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+                           className="mb-2
+                           text-sm
+                           font-medium
+                           text-gray-900
+                           sr-only
+                           dark:text-white">Your Email</label>
+
+                    <button
+                            onClick={handleDropdownClick}
+                            id="dropdown-button"
+                            data-dropdown-toggle="dropdown"
+                            className="flex-shrink-0
+                            z-10
+                            inline-flex
+                            items-center
+                            py-2.5
+                            px-4
+                            text-sm
+                            font-medium
+                            text-center
+                            text-gray-900
+                            bg-gray-100
+                            border
+                            border-gray-300
+                            rounded-l-lg
+                            hover:bg-gray-200
+                            focus:ring-4
+                            focus:outline-none
+                            focus:ring-gray-100
+                            dark:bg-gray-700
+                            dark:hover:bg-gray-600
+                            dark:focus:ring-gray-700
+                            dark:text-white
+                            dark:border-gray-600"
+
                             type="button">All categories <svg aria-hidden="true" className="w-4 h-4 ml-1"
                                                               fill="currentColor" viewBox="0 0 20 20"
                                                               xmlns="http://www.w3.org/2000/svg">
@@ -17,8 +58,9 @@ export default function Explore() {
                               d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                               clip-rule="evenodd"></path>
                     </svg></button>
+
                     <div id="dropdown"
-                         className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                         className={`z-10 ${dropdownButtonIsClicked ? "" : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
                         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
                             <li>
                                 <button type="button"
@@ -42,6 +84,7 @@ export default function Explore() {
                             </li>
                         </ul>
                     </div>
+
                     <div className="relative w-full">
                         <input type="search" id="search-dropdown"
                                className="block
