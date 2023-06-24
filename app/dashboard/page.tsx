ad934048d9a1a9ae51ecd648e5c96169ace99b2e
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-06-24 18:43:37 +0200

    improved size of modules

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,6 +16,9 @@ const tempCharts = [
     {
         id: 4,
     },
+    {
+        id: 5,
+    },
 ]
 
 export default function dashboardModuleBoard() {
@@ -46,10 +49,10 @@ export default function dashboardModuleBoard() {
 
     return (
         <div className="">
-            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-0">
                 <h2 className="text-6xl">Dashboard</h2>
 
-                <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                <div className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-5">
                     {tempCharts.map((tempChart) => (
                         <a key="" href="" className="group">
                             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
