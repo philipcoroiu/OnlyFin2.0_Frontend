8ff0e3612e07d73dfdf952c042875abee888c940
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-06-25 17:36:29 +0200

    Added input fields to studio table

---
+++ app/studio/chartPreview/StudioPreviewChart.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import React, {useState} from "react";
+import HighchartsReact from "highcharts-react-official";
+import Highcharts from "highcharts";
+
+export default function StudioPreviewChart() {
+
+    const [studioChart, setStudioChart] = useState({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+
+
+    return(
+        <div>
+            <HighchartsReact
+                containerProps={{style: {height: '100%', weight: '100%'}}}
+                highcharts={Highcharts}
+                options={studioChart}
+            />
+        </div>
+    )
+}
