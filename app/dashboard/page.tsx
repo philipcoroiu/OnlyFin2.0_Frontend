aa101543b4b60d3c17a63c517a54df6f26d08b0c
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-06-25 12:49:32 +0200

    Rearranged file structure in dashbard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,64 +1,11 @@
 "use client"
-import HighchartsReact from "highcharts-react-official";
-import Highcharts from "highcharts";
 import React, {useState} from "react";
-import Tabs from "./tabs";
+import Tabs from "./Tabs";
+import DashboardModules from "@/app/dashboard/DashbordModules";
 
-const tempCharts = [
-    {
-        id: 1,
-    },
-    {
-        id: 2,
-    },
-    {
-        id: 3,
-    },
-    {
-        id: 4,
-    },
-    {
-        id: 5,
-    },
-    {
-        id: 6,
-    },
-    {
-        id: 7,
-    },
-    {
-        id: 8,
-    },
-]
+
 
 export default function dashboardModuleBoard() {
-
-    /////// TEMP ////////
-    const [studioChart, setStudioChart] = useState({
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
-    });
-    ////////////////
-
 
     const [activeStockTab, setActiveStockTab] = useState(0);
     const [activeCategoryTab, setActiveCategoryTab] = useState(0);
@@ -89,38 +36,8 @@ export default function dashboardModuleBoard() {
                         activeCategoryTab={activeCategoryTab}
                     ></Tabs>
 
-                    <div
-                        className="grid
-                    grid-cols-1
-                    gap-x-6
-                    gap-y-5
-                    sm:grid-cols-2
-                    lg:grid-cols-3
-                    xl:grid-cols-3
-                    xl:gap-x-5">
+                    <DashboardModules></DashboardModules>
 
-                        {tempCharts.map((tempChart) => (
-                            <a key="" className="group">
-
-                                <div
-                                    className="aspect-h-1
-                                aspect-w-1
-                                w-full
-                                overflow-hidden
-                                rounded-lg
-                                bg-gray-200
-                                xl:aspect-h-8
-                                xl:aspect-w-7">
-
-                                    <HighchartsReact
-                                        containerProps={{style: {height: '100%', weight: '100%'}}}
-                                        highcharts={Highcharts}
-                                        options={studioChart}
-                                    />
-                                </div>
-                            </a>
-                        ))}
-                    </div>
                 </div>
             </div>
         </div>
