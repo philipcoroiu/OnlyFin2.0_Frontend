889d356d2ed02ced21ef445058fb01c6c999c2fc
JakobEdvardsson <jakob.edvardsson@outlook.com>
2023-06-26 13:22:52 +0200

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -51,93 +51,139 @@ export default function register() {
     return (
         <div className="
         flex
+        items-center
         justify-center
-        mx-[5%]
-        mt-5
-        dark:bg-gray-800
-        rounded-lg
-        p-3"
-        >
-            <form onSubmit={handleSubmit} className="">
+        p-12">
+            <form
+                onSubmit={handleSubmit}>
+
                 <div className="
-                flex
-                flex-col
-                justify-center">
-                    <h1 className="
-                    text-3xl
-                    font-bold
-                    text-center"
-                    >Register</h1>
+                max-w-xl
+                rounded-3xl
+                bg-gradient-to-b
+                from-sky-300
+                to-purple-500
+                p-px dark:from-gray-800
+                dark:to-transparent">
+
+                    <div className="
+                    rounded-[calc(1.5rem-1px)]
+                    bg-white
+                    px-10 p-12
+                    dark:bg-gray-900">
+
+                        <div>
+                            <h1 className="text-xl font-semibold text-gray-800 dark:text-white">Register an account</h1>
+                            <p className="text-sm tracking-wide text-gray-600 dark:text-gray-300"><span>Already a user? </span>
+                                <Link className="text-blue-600 transition duration-200 hover:underline dark:text-blue-400" href={"../login"}>
+                                    Login!
+                                </Link>
+                            </p>
+                        </div>
+
+                        <div className="mt-8 space-y-6">
+                            <div className="space-y-6">
+
+                                <input className="w-full
+                                bg-transparent
+                                text-gray-600
+                                dark:text-white
+                                dark:border-gray-700
+                                rounded-md
+                                border
+                                border-gray-300
+                                px-3
+                                py-2
+                                text-sm
+                                placeholder-gray-600
+                                invalid:border-red-500
+                                dark:placeholder-gray-300
+                                "
+                                       type="text"
+                                       id="username"
+                                       name="username"
+                                       value={username}
+                                       onChange={handleUsernameChange}
+                                       placeholder="Username"
+                                       maxLength={50}
+                                />
+
+
+                                <input className="w-full
+                                bg-transparent
+                                text-gray-600
+                                dark:text-white
+                                dark:border-gray-700
+                                rounded-md
+                                border
+                                border-gray-300
+                                px-3
+                                py-2
+                                text-sm
+                                placeholder-gray-600
+                                invalid:border-red-500
+                                dark:placeholder-gray-300
+                                "
+                                       type="email"
+                                       id="username"
+                                       name="username"
+                                       value={username}
+                                       onChange={handleUsernameChange}
+                                       placeholder="Email"
+                                       maxLength={50}
+                                />
+
+                                <input className="w-full
+                                bg-transparent
+                                text-gray-600
+                                dark:text-white
+                                dark:border-gray-700
+                                rounded-md
+                                border
+                                border-gray-300
+                                px-3
+                                py-2
+                                text-sm
+                                placeholder-gray-600
+                                invalid:border-red-500
+                                dark:placeholder-gray-300
+
+                                "
+                                       type="password"
+                                       id="password"
+                                       name="password"
+                                       value={password}
+                                       onChange={handlePasswordChange}
+                                       placeholder="Password"
+                                       maxLength={100}
+                                />
+                            </div>
+
+                            <button
+                                className="
+                                h-10
+                                px-3
+                                w-full
+                                text-white
+                                text-center
+
+                                bg-blue-700
+                                hover:bg-blue-800
+                                focus:ring-4
+                                focus:ring-blue-300
+                                font-medium
+                                rounded-lg
+                                text-lg
+                                dark:bg-blue-600
+                                dark:hover:bg-blue-700
+                                focus:outline-none
+                                dark:focus:ring-blue-800
+                                ">
+                                Register
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className="
-                flex
-                flex-col
-                m-2"
-                >
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={username}
-                        onChange={handleUsernameChange}
-                        placeholder="Username"
-                        maxLength={50}
-                        className="
-                        m-2
-                        p-2
-                        rounded-md"
-                    />
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                        placeholder="Email"
-                        maxLength={50}
-                        className="
-                        m-2
-                        p-2
-                        rounded-md"
-                    />
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                        placeholder="Password"
-                        maxLength={100}
-                        className="
-                        m-2
-                        p-2
-                        rounded-md"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="
-                    text-white
-                    bg-blue-700
-                    hover:bg-blue-800
-                    focus:ring-4
-                    focus:ring-blue-300
-                    font-medium
-                    rounded-lg
-                    text-sm px-4
-                    lg:px-5 py-2
-                    lg:py-2.5
-                    mr-2
-                    dark:bg-blue-600
-                    dark:hover:bg-blue-700
-                    focus:outline-none
-                    dark:focus:ring-blue-800"
-                >
-                    Register
-                </button>
-                <Link href={"../Login"}>
-                    Already a user? Login here.
-                </Link>
             </form>
         </div>
     )
