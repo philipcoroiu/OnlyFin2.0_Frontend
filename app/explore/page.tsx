f8bbaa88664c9ee547f55970b1c07e716e1d511e
JakobEdvardsson <jakob.edvardsson@outlook.com>
2023-06-27 13:36:21 +0200

    Update explore page

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -11,27 +11,27 @@ export default function Explore() {
     }
 
     return (
-        <div>
+        <div className="flex  justify-center">
 
-            <form >
-                <div className="flex w-full max-w-xl mt-12 mx-auto
+            <form className="m-4">
+                <div className="flex w-full max-w-xl mt-12 border-2 border-amber-800
                 shadow-xl shadow-gray-600/[0.3] rounded-lg
                 dark:shadow-none
 
-                " >
-                    <label htmlFor="search-dropdown"
+                ">
+                    {/*<label htmlFor="search-dropdown"
                            className="mb-2
                            text-sm
                            font-medium
                            text-gray-900
                            sr-only
-                           dark:text-white">Your Email</label>
+                           dark:text-white">Your Email</label>*/}
 
                     <button
-                            onClick={handleDropdownClick}
-                            id="dropdown-button"
-                            data-dropdown-toggle="dropdown"
-                            className="flex-shrink-0
+                        onClick={handleDropdownClick}
+                        id="dropdown-button"
+                        data-dropdown-toggle="dropdown"
+                        className="flex-shrink-0
                             z-10
                             inline-flex
                             items-center
@@ -55,13 +55,23 @@ export default function Explore() {
                             dark:text-white
                             dark:border-gray-600"
 
-                            type="button">All categories <svg aria-hidden="true" className="w-4 h-7 ml-1"
-                                                              fill="currentColor" viewBox="0 0 20 20"
-                                                              xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
+                        type="button">
+                        <span className="
+                        hidden
+                        xsm:block
+                        "
+                        >All categories</span>
+
+
+                        <svg aria-hidden="true" className="w-4 h-7 ml-1"
+                                                          fill="currentColor" viewBox="0 0 20 20"
+                                                          xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd"
                               d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                              clip-rule="evenodd"></path>
-                    </svg></button>
+                              clipRule="evenodd"></path>
+                    </svg>
+
+                    </button>
 
                     <div id="dropdown"
                          className={`z-10 ${dropdownButtonIsClicked ? "" : "hidden"} origin-top-right absolute mt-12 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
@@ -89,7 +99,7 @@ export default function Explore() {
                         </ul>
                     </div>
 
-                    <div className="relative w-full">
+                    <div className=" w-full flex">
                         <input type="search" id="search-dropdown"
                                className="block
                                p-2.5
@@ -98,7 +108,6 @@ export default function Explore() {
                                text-sm
                                text-gray-900
                                bg-gray-50
-                               rounded-r-lg
                                border
                                border-l-0
                                border-blue-700/[0.7]
@@ -112,17 +121,19 @@ export default function Explore() {
                                dark:focus:border-blue-500
                                "
                                placeholder="Search Mockups, Logos, Design Templates..." required/>
-                            <button type="submit"
-                                    className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                <svg aria-hidden="true" className="w-5 h-7" fill="none" stroke="currentColor"
-                                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                </svg>
-                                <span className="sr-only">Search</span>
-                            </button>
+
                     </div>
+                    <button type="submit"
+                            className=" p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg aria-hidden="true" className="w-5 h-7" fill="none" stroke="currentColor"
+                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                        <span className="sr-only">Search</span>
+                    </button>
                 </div>
+
             </form>
 
         </div>
