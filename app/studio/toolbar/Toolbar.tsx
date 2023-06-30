84cd3fccb375fdd9f5059bf9d3a325480a2b6886
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-06-30 09:59:54 +0200

    Added chart title field

--- app/studio/toolbar/Toolbar.tsx
+++ app/studio/toolbar/Toolbar.tsx
@@ -1,10 +1,44 @@
 "use client"
-import RemoveThisClassLater from "@/app/studio/toolbar/toolbarTable/RemoveThisClassLater";
 import ToolbarTable from "@/app/studio/toolbar/toolbarTable/ToolbarTable";
 
 export default function Toolbar() {
     return(
         <div>
+
+            {
+                // ************//
+                // CHART TITLE //
+                // ************//
+            }
+
+            <div className="mb-6">
+                <label htmlFor="default-input"
+                       className="block
+                       mb-2
+                       text-sm
+                       font-medium
+                       text-gray-900
+                       dark:text-white">Chart Title</label>
+
+                <input type="text"
+                       id="default-input"
+                       className="bg-gray-50
+                       border
+                       border-gray-300
+                       text-gray-900
+                       text-sm rounded-lg
+                       focus:ring-blue-500
+                       focus:border-blue-500
+                       block
+                       w-full
+                       p-2.5
+                       dark:bg-gray-700
+                       dark:border-gray-600
+                       dark:placeholder-gray-400
+                       dark:text-white
+                       dark:focus:ring-blue-500
+                       dark:focus:border-blue-500"/>
+            </div>
 
             {
                 // ***************//
