90bdc67f8ea6cd8a4d21e0f37325e3a1c188a41b
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-07-01 14:03:57 +0200

    added stock and category field

--- app/studio/toolbar/startField/StartField.tsx
+++ app/studio/toolbar/startField/StartField.tsx
@@ -43,12 +43,81 @@ export default function StartField(props : any) {
 
             {
                 // ***************//
+                // STOCK SELECTOR //
+                // ***************//
+            }
+
+            <label htmlFor="stocks" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
+                a stock</label>
+
+            <select id="stocks"
+                    className="
+                    bg-gray-50
+                    border
+                    border-gray-300
+                    text-gray-900
+                    text-sm rounded-lg
+                    focus:ring-blue-500
+                    focus:border-blue-500
+                    block
+                    w-full
+                    p-2.5
+                    dark:bg-gray-700
+                    dark:border-gray-600
+                    dark:placeholder-gray-400
+                    dark:text-white
+                    ark:focus:ring-blue-500
+                    dark:focus:border-blue-500">
+
+                <option selected>Stock</option>
+                <option value="bar">Bar</option>
+                <option value="column">Column</option>
+                <option value="line">Line</option>
+            </select>
+
+            {
+                // ******************//
+                // CATEGORY SELECTOR //
+                // ******************//
+            }
+
+            <label htmlFor="stocks" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
+                a category</label>
+
+            <select id="stocks"
+                    className="
+                    bg-gray-50
+                    border
+                    border-gray-300
+                    text-gray-900
+                    text-sm rounded-lg
+                    focus:ring-blue-500
+                    focus:border-blue-500
+                    block
+                    w-full
+                    p-2.5
+                    dark:bg-gray-700
+                    dark:border-gray-600
+                    dark:placeholder-gray-400
+                    dark:text-white
+                    ark:focus:ring-blue-500
+                    dark:focus:border-blue-500">
+
+                <option selected>Category</option>
+                <option value="bar">Bar</option>
+                <option value="column">Column</option>
+                <option value="line">Line</option>
+            </select>
+
+
+            {
+                // ***************//
                 // CHART SELECTOR //
                 // ***************//
             }
 
             <label htmlFor="charts" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
-                an option</label>
+                a chart type</label>
 
             <select id="charts"
                     onChange={props.handleChartSelectChange}
