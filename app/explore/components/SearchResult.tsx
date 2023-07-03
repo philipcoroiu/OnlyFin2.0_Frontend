03fcc4411e31b949df673b0e015c5a6097a8f445
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-07-03 19:13:40 +0200

    Fixed design formatting error

--- app/explore/components/SearchResult.tsx
+++ app/explore/components/SearchResult.tsx
@@ -5,43 +5,31 @@ import LoadingAnimation from "@/app/components/LoadingAnimation";
 import SearchProfile from "@/app/explore/components/searchProfile/SearchProfile";
 
 
-export default function SearchResult(props : any) {
+export default function SearchResult({searchResult} : any) {
 
     function renderSearchResult() {
-        if (props.searchResult) {
-            return (
-
-                props.searchResult.map((data: any, index: number) => {
-                    return (
-                        <SearchProfile
-                            username={props.searchResult[index].username}
-                        />
-                    );
-                })
-            )
-
-        } else {
-            return (
-                <LoadingAnimation/>
-            )
-        }
+        return (
+            searchResult.map((data: any, index: number) => {
+                return (
+                    <SearchProfile
+                        username={searchResult[index].username}
+                    />
+                );
+            })
+        )
     }
 
     return(
-        <div className="mx-auto
-                        my-10
-                        px-4
-                        sm:px-6
-                        lg:px-20">
-
-            <div className="grid
-                            grid-cols-
-                            md:grid-cols-4
-                            gap-4">
-
-            {renderSearchResult()}
-
-            </div>
+        <div>
+            {searchResult ? (
+                    <div className="mx-auto my-10 px-4 sm:px-6 lg:px-20">
+                        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                            {renderSearchResult()}
+                        </div>
+                    </div>
+                ) : (
+                    <LoadingAnimation />
+                )}
         </div>
     )
 }
