345d1e8e5cb2377f97eb4d0b5801f1f315fdc1c8
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-07-03 18:22:25 +0200

    Added subscribe button and reformatted code

--- app/explore/components/SearchResult.tsx
+++ app/explore/components/SearchResult.tsx
@@ -9,10 +9,39 @@ export default function SearchResult(props : any) {
         if (props.searchResult) {
             return props.searchResult.map((data: any, index: number) => {
                 return (
-                    <div key={index} className="bg-gray-700 rounded-lg p-4">
+                    <div
+                        key={index}
+                        className="bg-gray-700
+                    rounded-lg
+                    p-4">
+
                         <Avatar/>
-                        <h3 className="text-xl font-bold">{props.searchResult[index].username}</h3>
-                        <p></p>
+                        <h3
+                            className="text-xl
+                            font-bold">
+                            {props.searchResult[index].username}
+                        </h3>
+
+                        <button
+                            type="button"
+                            className="text-white
+                            bg-blue-700
+                            hover:bg-blue-800
+                            focus:ring-4
+                            focus:ring-blue-300
+                            font-medium
+                            rounded-lg
+                            text-sm
+                            px-5
+                            py-2.5
+                            mr-2
+                            mb-2
+                            dark:bg-blue-600
+                            dark:hover:bg-blue-700
+                            focus:outline-none
+                            dark:focus:ring-blue-800">
+                            Subscribe
+                        </button>
                     </div>
                 );
             });
@@ -22,29 +51,22 @@ export default function SearchResult(props : any) {
     }
 
     return(
-        <div className="mx-auto my-10 px-4 sm:px-6 lg:px-20">
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                <div className="bg-gray-200 rounded-lg p-4">
-                    <h3 className="text-xl font-bold">Card 1</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="bg-gray-200 rounded-lg p-4">
-                    <h3 className="text-xl font-bold">Card 2</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="bg-gray-200 rounded-lg p-4">
-                    <h3 className="text-xl font-bold">Card 3</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="bg-gray-200 rounded-lg p-4">
-                    <h3 className="text-xl font-bold">Card 4</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
+        <div
+            className="mx-auto
+            my-10
+            px-4
+            sm:px-6
+            lg:px-20">
+
+            <div
+                className="grid
+                grid-cols-2
+                md:grid-cols-4
+                gap-4">
 
                 {renderSearchResult()}
 
             </div>
         </div>
-
     )
 }
