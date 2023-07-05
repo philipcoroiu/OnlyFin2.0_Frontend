6bd56a40beac2cc373d45a7c5e471db8cfd60e28
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-07-05 13:44:35 +0200

    Added log out button to header

--- app/components/header/headerComponents/LoggedInButtons.tsx
+++ app/components/header/headerComponents/LoggedInButtons.tsx
@@ -4,11 +4,33 @@ import Avatar from "@/app/components/Avatar";
 
 export default function LoggedInButtons() {
     return (
-        <a href="/" className="flex items-center">
-            <div className="mr-1">
-                <Avatar/>
-            </div>
-            <p className="ml-2">Username</p>
-        </a>
+        <div className="flex items-center">
+            <a href="/" className="flex items-center">
+                <div className="mr-1">
+                    <Avatar/>
+                </div>
+                <p className="ml-2">Username</p>
+            </a>
+            <a href="/login"
+               className="
+                           text-gray-800
+                           dark:text-white
+                           hover:bg-gray-50
+                           focus:ring-4
+                           focus:ring-gray-300
+                           font-medium
+                           rounded-lg
+                           text-sm
+                           px-4
+                           lg:px-5
+                           py-2
+                           lg:py-2.5
+                           mr-2
+                           dark:hover:bg-gray-700
+                           focus:outline-none
+                           dark:focus:ring-gray-800
+                           ml-5"
+            >Log out</a>
+        </div>
     )
 }
