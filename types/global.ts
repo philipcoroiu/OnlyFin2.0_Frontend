db7cca8bb1b580de87fba87686da04791fb9bc0f
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-07-13 22:22:34 +0200

    Added global types file for defining types globally

---
+++ types/global.ts
@@ -0,0 +1,20 @@
+type OnlyfinModule = {
+    targetCategoryId: number,
+    height: number,
+    width: number,
+    xAxis: number,
+    yAxis: number,
+    type: string,
+    content: JSON
+}
+
+type OnlyfinProfile = {
+    id: number,
+    username: string
+}
+
+type OnlyfinProfileSubInfo = {
+    id: number,
+    username: string,
+    isSubscribed: boolean
+}
