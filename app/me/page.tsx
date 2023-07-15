ee94ca92a01704bddc23bc025f021c10225fe269
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-07-15 14:21:01 +0200

    Make "me" page redirect to login if not logged in

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -3,17 +3,27 @@
 import {useEffect, useState} from "react";
 import {ApiCalls} from "@/app/utilities/ApiCalls";
 import MyProfile from "@/app/me/components/MyProfile";
+import {useRouter} from "next/navigation";
 
 export default function Page() {
 
     const [username, setUsername] = useState<string>("")
 
+    const router = useRouter()
+
     useEffect(() => {
         ApiCalls.whoAmI()
             .then(response => {
+                if (response.status === 204) {
+                    router.push("/login?redirect=me")
+                }
+
                 const data: string = response.data
 
                 setUsername(data)
+            })
+            .catch(error => {
+                console.log("[me/page.tsx]: " + error)
             })
     }, [])
 
