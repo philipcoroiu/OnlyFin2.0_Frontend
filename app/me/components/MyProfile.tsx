1b45a9916c5635060fac348e7d8869d210805418
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2023-07-16 13:24:33 +0200

    bio added
    api calls needs to be implemented

--- app/me/components/MyProfile.tsx
+++ app/me/components/MyProfile.tsx
@@ -1,10 +1,21 @@
 "use client"
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {ApiCalls} from "@/app/utilities/ApiCalls";
 
 export default function MyProfile({username}: any) {
 
     const [edit, setEdit] = useState(false)
+
+    const[aboutMe, setAboutMe] = useState("")
+
+    function changeEdit () {
+        setEdit(!edit)
+    }
+
+    function setText (event : any) {
+        setAboutMe(event.target.value)
+    }
 
     return (
         <div className="flex">
@@ -16,29 +27,52 @@ export default function MyProfile({username}: any) {
                 ml-5
                 mt-5"
             ></div>
-            <div className={""}>
-                <h1 className="text-xl font-bold ">{username}</h1>
-                {edit ? <textarea id="message" className="
-                                                    block
-                                                    p-2.5
-                                                    w-full
-                                                    text-sm
-                                                    text-gray-900
-                                                    bg-gray-50
-                                                    rounded-lg
-                                                    border
-                                                    border-gray-300
-                                                    focus:ring-blue-500
-                                                    focus:border-blue-500
-                                                    dark:bg-gray-700
-                                                    dark:border-gray-600
-                                                    dark:placeholder-gray-400
-                                                    dark:text-white
-                                                    dark:focus:ring-blue-500
-                                                    dark:focus:border-blue-500"
-                              placeholder="Write your thoughts here..."></textarea>
+            <div className="
+                    bg-yellow-300
+                    w-4/5
+                    h-52
+                    flex
+                    flex-col
+                    "
+            >
+                <h1 className="text-xl font-bold m-5 mt-10 bg-white">{username}</h1>
+                {edit ?
+                    <div className="bg-red-300">
+
+                        <form>
+                            <div
+                                className="w-4/5 ml-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                                <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+                                    <label htmlFor="comment" className="sr-only">Your comment</label>
+                                    <textarea id="comment"
+                                              className="w-full px-0 text-sm text-gray-900 bg-white border-0 outline-none dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                                              value={aboutMe}
+                                              onSubmit={setText(value)}
+                                              placeholder="Write a bio..." ></textarea>
+                                </div>
+                                <div
+                                    className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+                                    <button type="submit"
+                                            className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+                                            onClick={changeEdit}
+                                    >
+                                        Save bio
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
                     :
-                    <p>here will be bio</p>
+                    <div>
+                        <p className="bg-red-300 w-5/6 ml-5">here will be bio</p>
+                        <button
+                                className="inline-flex items-center ml-5 mt-5 py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+                                onClick={changeEdit}
+                        >
+                            Edit bio
+                        </button>
+                    </div>
                 }
 
             </div>
