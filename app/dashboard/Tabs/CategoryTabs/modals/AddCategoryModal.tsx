43d912ece9785024cdd6705b2e53c7d6a1d4bbdb
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-07-18 17:46:01 +0200

    some simple reformatting

--- app/dashboard/Tabs/CategoryTabs/modals/AddCategoryModal.tsx
+++ app/dashboard/Tabs/CategoryTabs/modals/AddCategoryModal.tsx
@@ -29,11 +29,33 @@ export default function AddCategoryModal(props : any) {
 
                     {/* Modal Content */}
 
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <div className="relative
+                                    bg-white
+                                    rounded-lg
+                                    shadow
+                                    dark:bg-gray-700">
+
                         <button type="button"
-                                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                className="absolute
+                                            top-3
+                                            right-2.5
+                                            text-gray-400
+                                            bg-transparent
+                                            hover:bg-gray-200
+                                            hover:text-gray-900
+                                            rounded-lg
+                                            text-sm
+                                            w-8
+                                            h-8
+                                            ml-auto
+                                            inline-flex
+                                            justify-center
+                                            items-center
+                                            dark:hover:bg-gray-600
+                                            dark:hover:text-white"
                                 data-modal-hide="crypto-modal"
                                 onClick={props.handleExitButtonClick}>
+
                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 14 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
@@ -44,8 +66,18 @@ export default function AddCategoryModal(props : any) {
 
                         {/* Modal Header */}
 
-                        <div className="px-6 py-4 border-b rounded-t dark:border-gray-600">
-                            <h3 className="text-base font-semibold text-gray-900 lg:text-xl dark:text-white">
+                        <div className="px-6
+                                        py-4
+                                        border-b
+                                        rounded-t
+                                        dark:border-gray-600">
+
+                            <h3 className="text-base
+                                            font-semibold
+                                            text-gray-900
+                                            lg:text-xl
+                                            dark:text-white">
+                                
                                 Name your category
                             </h3>
                         </div>
