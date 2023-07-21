ab445cf1efe8dc096e4cfb9f06097482cd62d504
JakobEdvardsson <jakob.edvardsson@outlook.com>
2023-07-21 16:27:49 +0200

    Update SearchProfileSkeleton.tsx
    Co-Authored-By: Johannes <98045373+Luxcorel@users.noreply.github.com>

--- app/explore/components/searchProfile/SearchProfileSkeleton.tsx
+++ app/explore/components/searchProfile/SearchProfileSkeleton.tsx
@@ -6,7 +6,9 @@ export default function SearchProfileSkeleton() {
 
     return (
         <div className="animate-pulse bg-gray-50 rounded-lg p-4 dark:bg-gray-700">
-            <Avatar/>
+            <div className={"w-20 h-20"}>
+                <Avatar/>
+            </div>
         </div>
     )
 }
