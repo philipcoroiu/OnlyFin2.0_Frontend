a10fd4ba8e7ca3e871db6b1ef6606b9fc748fe69
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-07-25 16:04:07 +0200

    Sweet baby jesus i fixed the studio

--- app/studio/toolbar/toolbarTable/ToolbarTable.tsx
+++ app/studio/toolbar/toolbarTable/ToolbarTable.tsx
@@ -2,38 +2,50 @@ import Spreadsheet from "react-spreadsheet";
 import { useState } from "react";
 
 export default function ToolbarTable(props : any) {
-    const initialData: TableCell = [
-        [{ value: "Vanilla" }, { value: "Chocolate" }],
-        [{ value: "Strawberry" }, { value: "Cookies" }]
-    ];
 
-    const [spreadsheetData, setSpreadsheetData] = useState(initialData);
+    const initStudioSpreadsheetData = [
+        [{ value: 'Year' }, { value: "Mike" }, { value: "John" }, { value: "Anna" }],
+        [{ value: '2020' }, { value: 2 }, { value: 3 }, { value: 2 }],
+        [{ value: '2021' }, { value: 2 }, { value: 3 }, { value: 2 }],
+        [{ value: '2022' }, { value: 3 }, { value: 2 }, { value: 3 }],
+    ]
 
-    //TODO: Replace this function and the state with the prop function
+    const [spreadsheetData, setSpreadsheetData] = useState(initStudioSpreadsheetData);
+
     function handleSpreadsheetChange(newData : any) {
-        setSpreadsheetData(newData);
+        //setSpreadsheetData(newData);
         props.handleChartDataChange(newData)
     }
 
     function handleAddColumnClick() {
         props.setChartData((prevData: TableCell) => prevData.map(row => [...row, { value: "" }]));
+        setSpreadsheetData((prevData: any) => prevData.map((row: any) => [...row, { value: "" }]))
     }
 
     function handleRemoveColumnClick() {
         props.setChartData((prevData: TableCell) => prevData.map(row => row.slice(0, -1)));
+        setSpreadsheetData((prevData: any) => prevData.map((row: any) => row.slice(0, -1)));
     }
 
     function handleAddRowClick() {
         const newRow = new Array(spreadsheetData[0].length).fill({ value: "" });
         props.setChartData((prevData: TableCell) => [...prevData, newRow]);
+        setSpreadsheetData((prevData: any) => [...prevData, newRow])
     }
 
     function handleRemoveRowClick() {
+        /*
         props.setChartData((prevData: TableCell) => {
             const newData = [...prevData];
             newData.pop();
             return newData;
         });
+        */
+        setSpreadsheetData((prevData: any) => {
+            const newData = [...prevData];
+            newData.pop();
+            return newData;
+        })
     }
 
     return (
@@ -159,7 +171,7 @@ export default function ToolbarTable(props : any) {
                 // ******************//
             }
 
-            <Spreadsheet data={props.chartData} onChange={handleSpreadsheetChange} />
+            <Spreadsheet data={spreadsheetData} onChange={handleSpreadsheetChange} />
         </div>
     );
 }
