2db60fe1c7557747a87ce36d1fc34f14e63a7cfb
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-07-26 17:39:43 +0200

    Added yaxis and xaxis input fields

--- app/studio/toolbar/startField/StartField.tsx
+++ app/studio/toolbar/startField/StartField.tsx
@@ -213,6 +213,82 @@ export default function StartField(props : any) {
                 <option value="column">Column</option>
                 <option value="line">Line</option>
             </select>
+
+            {
+                // *************//
+                // Y-AXIS INPUT //
+                // *************//
+            }
+
+            <div className="mb-6">
+                <label htmlFor="default-input"
+                       className="block
+                       mb-2
+                       text-sm
+                       font-medium
+                       text-gray-900
+                       dark:text-white">Y axis title</label>
+
+                <input type="text"
+                       id="y-axis-input"
+                       placeholder="Y axis"
+                       maxLength={60}
+                       //onChange={props.handleChartTitleChange}
+                       className="bg-gray-50
+                       border
+                       border-gray-300
+                       text-gray-900
+                       text-sm rounded-lg
+                       focus:ring-blue-500
+                       focus:border-blue-500
+                       block
+                       w-full
+                       p-2.5
+                       dark:bg-gray-700
+                       dark:border-gray-600
+                       dark:placeholder-gray-400
+                       dark:text-white
+                       dark:focus:ring-blue-500
+                       dark:focus:border-blue-500"/>
+            </div>
+
+            {
+                // *************//
+                // X-AXIS INPUT //
+                // *************//
+            }
+
+            <div className="mb-6">
+                <label htmlFor="default-input"
+                       className="block
+                       mb-2
+                       text-sm
+                       font-medium
+                       text-gray-900
+                       dark:text-white">X axis input</label>
+
+                <input type="text"
+                       id="x-axis-input"
+                       placeholder="X axis"
+                       maxLength={60}
+                       //onChange={props.handleChartTitleChange}
+                       className="bg-gray-50
+                       border
+                       border-gray-300
+                       text-gray-900
+                       text-sm rounded-lg
+                       focus:ring-blue-500
+                       focus:border-blue-500
+                       block
+                       w-full
+                       p-2.5
+                       dark:bg-gray-700
+                       dark:border-gray-600
+                       dark:placeholder-gray-400
+                       dark:text-white
+                       dark:focus:ring-blue-500
+                       dark:focus:border-blue-500"/>
+            </div>
         </div>
     )
 }
