1f330a313619fc8f0662c40b21728ce27866dc13
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-08-05 15:10:05 +0200

    Polishing

--- app/dashboard/modulesContainer/Module.tsx
+++ app/dashboard/modulesContainer/Module.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 type Props = {
     moduleData: OnlyfinModule,
-    isProfileDashboard: boolean
+    isProfileDashboard?: boolean
 }
 
 export default function Module(props: Props){
